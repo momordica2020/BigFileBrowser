27f7718f6bd2afcb0ef153f5b27752ecd8b65511
hontsev <hontsev@gmail.com>
2017-10-11 14:39:56 +0800

    files

---
+++ BigFileBrowser/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BigFileBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 116);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(886, 354);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "文件：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "位置：";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(197, 54);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(702, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.TickFrequency = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(151, 89);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "每次显示字符数：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "utf8",
+            "gb2312"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 482);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "大文件查看";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ BigFileBrowser/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Mozilla.NUniversalCharDet;
+
+namespace BigFileBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        string filepath = "";
+        long filelen = 1;
+        Encoding encoding=Encoding.UTF8;
+
+        ///// <summary>
+        ///// 返回流的编码格式
+        ///// </summary>
+        ///// <param name="stream"></param>
+        ///// <returns></returns>
+        //private static Encoding getEncoding(string streamName)
+        //{
+        //    Encoding encoding = Encoding.Default;
+        //    using (Stream stream = new FileStream(streamName, FileMode.Open))
+        //    {
+        //        MemoryStream msTemp = new MemoryStream();
+        //        int len = 0;
+        //        byte[] buff = new byte[512];
+        //        while ((len = stream.Read(buff, 0, 512)) > 0)
+        //        {
+        //            msTemp.Write(buff, 0, len);
+        //        }
+        //        if (msTemp.Length > 0)
+        //        {
+        //            msTemp.Seek(0, SeekOrigin.Begin);
+        //            byte[] PageBytes = new byte[msTemp.Length];
+        //            msTemp.Read(PageBytes, 0, PageBytes.Length);
+        //            msTemp.Seek(0, SeekOrigin.Begin);
+        //            int DetLen = 0;
+        //            UniversalDetector Det = new UniversalDetector(null);
+        //            byte[] DetectBuff = new byte[4096];
+        //            while ((DetLen = msTemp.Read(DetectBuff, 0, DetectBuff.Length)) > 0 && !Det.IsDone())
+        //            {
+        //                Det.HandleData(DetectBuff, 0, DetectBuff.Length);
+        //            }
+        //            Det.DataEnd();
+        //            if (Det.GetDetectedCharset() != null)
+        //            {
+        //                encoding = Encoding.GetEncoding(Det.GetDetectedCharset());
+        //            }
+        //        }
+        //        msTemp.Close();
+        //        msTemp.Dispose();
+        //        return encoding;
+        //    }
+        //}
+        
+        private void getFileLen(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            filelen = fi.Length;
+        }
+
+        private void open(string path)
+        {
+            this.filepath = path;
+            label1.Text = "文件名：" + path;
+            getFileLen(path);
+            read(filelen / 1000 * trackBar1.Value);
+        }
+
+        private void read(long beginindex)
+        {
+            try
+            {
+                //Encoding encoding = Encoding.GetEncoding(getEncoding(filepath).BodyName);
+                using (FileStream fs = File.Open(filepath, FileMode.Open))
+                {
+                    fs.Seek(beginindex, SeekOrigin.Begin);
+
+                    int len = int.Parse(numericUpDown1.Value.ToString());
+                    byte[] buffer = new byte[len];
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+
+                        int read = fs.Read(buffer,0, buffer.Length);
+                        ms.Write(buffer, 0, read);
+                        string res = encoding.GetString(ms.ToArray());
+                        textBox1.Text = res;
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            open(openFileDialog1.FileName);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            read(filelen / 1000* trackBar1.Value );
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    encoding = Encoding.UTF8;
+                    break;
+                case 1:
+                    encoding = Encoding.GetEncoding("gb2312");
+                    break;
+                default:
+                    break;
+            }
+            read(filelen / 1000 * trackBar1.Value);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+            comboBox1.Select(0, 1);
+        }
+    }
+}

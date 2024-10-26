ac053c3cf3d4e52cf80e836340d916acdb084f24
momordica2020 <287859992@qq.com>
2024-10-26 13:18:41 +0800

    remove unused reference

--- BigFileBrowser/Form1.cs
+++ BigFileBrowser/Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using Mozilla.NUniversalCharDet;
 using System.Text.RegularExpressions;
 using BigFileBrowser.Properties;
 using System.Threading;

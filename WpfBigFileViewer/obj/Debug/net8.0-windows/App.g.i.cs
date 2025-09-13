035149cb8a061c6a4909e4c78e7d8f28ecb57282
momordica2020 <287859992@qq.com>
2025-09-13 13:44:42 +0800

    ignores

--- WpfBigFileViewer/obj/Debug/net8.0-windows/App.g.i.cs
+++ WpfBigFileViewer/obj/Debug/net8.0-windows/App.g.i.cs
@@ -52,7 +52,7 @@ namespace FileViewer {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
         public static void Main() {
             FileViewer.App app = new FileViewer.App();
             app.Run();
